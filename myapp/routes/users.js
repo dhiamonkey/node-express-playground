c490f642ec7d2353da98206a32ac86b004e94ecf
dhiamonkey <dhiamuhammad@gmail.com>
2022-05-23 12:45:55 +0700

    second commit

---
+++ myapp/routes/users.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var router = express.Router();
+var data = require("../db/dummy.json");
+const fs = require("fs");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.put("/user", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+
+router.delete("/user", (req, res) => {
+  res.send("Got a DELETE request at /user");
+});
+
+router.get("/data", (req, res) => {
+  res.send(data.user);
+});
+
+router.get("/data/:user", (req, res) => {
+  res.send(
+    data.user.includes(req.params.user)
+      ? `hi ${req.params.user}`
+      : "no known user found"
+  );
+});
+
+router.post("/data", (req, res) => {
+  console.log(req.body.user);
+  fs.writeFile(
+    // Path
+    "db/dummy.json",
+    // Content, do not delete stringify
+    JSON.stringify({ user: [...data.user, req.body.user] }),
+    // Options
+    {
+      encoding: "utf8",
+      flag: "w",
+    },
+    //Err Callback
+    function writeJSON(err) {
+      if (err) return console.log(err);
+    }
+  );
+  res.send(req.body);
+});
+
+module.exports = router;
